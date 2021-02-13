d9349adde620e2487db1f9552baf4d09fdb09fc7
swelbourn <welbourn.stephen@gmail.com>
2021-02-13 05:12:39 -0600

    add shop page

--- src/App.js
+++ src/App.js
@@ -3,26 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const TopicsList = () => (
-  <div>
-    <h1>Topics list.</h1>
-  </div>
-);
-
-const TopicDetail = () => (
-  <div>
-    <h1>Topic detail.</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/topics" component={TopicsList} />
-        <Route exact path="/topics/:topicId" component={TopicDetail} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
